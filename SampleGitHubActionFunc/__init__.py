9e49b181d39723c326b81284ecbbf3bec6e9a053
Michael Muruthi <mimuruth@microsoft.com>
2023-01-18 20:39:46 -0600

    update init file

--- SampleGitHubActionFunc/__init__.py
+++ SampleGitHubActionFunc/__init__.py
@@ -5,6 +5,7 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Done.')
 
     name = req.params.get('name')
     if not name:
